13445785c619202489244444b6952d17808e4021
Claudio Silva Bispo <110735259+Claubis@users.noreply.github.com>
2024-11-19 15:20:35 -0300

    Criação da estrutura do projeto para o projeto da faculdade

---
+++ Infrastructure/Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using ProjetoMvc.Models; // Ajuste o namespace conforme necessário
+
+namespace ProjetoMvc.Infrastructure.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> T_Usuario { get; set; }
+
+    }
+}
